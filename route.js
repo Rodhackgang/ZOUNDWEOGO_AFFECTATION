2b1d92874775a1979e1c94634b6033e25b212893
ZETREY <84082026+ZHKING13@users.noreply.github.com>
2024-09-15 09:36:40 +0000

    setup search route

--- route.js
+++ route.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 // Serve the index.html file for the root route
 router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index2.html'));
+});
+router.get('/add', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'));
 });
 
